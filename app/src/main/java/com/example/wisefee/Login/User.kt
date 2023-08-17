69892f360b37e13f0cfac924bb243c813be0f986
jinho7 <jh7524jh7524@gmail.com>
2023-08-17 20:16:13 +0900

    로그인

---
+++ app/src/main/java/com/example/wisefee/Login/User.kt
@@ -0,0 +1,8 @@
+package com.example.wisefee.Login
+
+import java.io.Serializable
+
+class User(
+    var grantType : String? = null,
+    var accessToken : String? = null
+): Serializable
--- app/src/main/java/com/example/wisefee/RetrofitService.kt
+++ app/src/main/java/com/example/wisefee/RetrofitService.kt
@@ -1,11 +1,23 @@
 package com.example.wisefee
 
 import com.example.wisefee.Cart.Product
+import com.example.wisefee.Login.User
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface RetrofitService {
     @GET("consumer/{cafeId}/product")
     fun getProductsForCafe(@Path("cafeId") cafeId: Int): Call<List<Product>>
+
+    @POST("/api/v1/auth/login")
+    @FormUrlEncoded
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<User>
+
 }
