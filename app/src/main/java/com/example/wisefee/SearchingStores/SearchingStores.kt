8e1974064048957f5eb435702d6d7352b1c33a87
insun <kis8294@gmail.com>
2023-11-09 22:07:31 +0900

    SearchingStore 구매 여부 팝업 추가

--- app/src/main/java/com/example/wisefee/SearchingStores/SearchingStores.kt
+++ app/src/main/java/com/example/wisefee/SearchingStores/SearchingStores.kt
@@ -3,8 +3,12 @@ package com.example.wisefee.SearchingStores
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.example.wisefee.MainActivity
+import com.example.wisefee.Menu.MenuActivity
 import com.example.wisefee.Mypage.MyPageActivity
 import com.example.wisefee.R
 import com.example.wisefee.Return.ReturnTumblerActivity
@@ -18,12 +22,56 @@ class SearchingStores : AppCompatActivity() {
         binding = ActivitySearchingStoresBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.storeButton.setOnClickListener {
+            showPurchaseConfirmationPopup()
+        }
+
+
         binding.home.setOnClickListener { startActivity(Intent(this, MainActivity::class.java)) }
         // 각각 Activity 들 여기에 연결해주세요.
         binding.rental.setColorFilter(ContextCompat.getColor(this, R.color.selection_color))
         binding.returnTumbler.setOnClickListener { startActivity(Intent(this, ReturnTumblerActivity::class.java)) }
         binding.mypage.setOnClickListener { startActivity(Intent(this, MyPageActivity::class.java)) }
 
+
+    }
+
+    private fun showPurchaseConfirmationPopup() {
+        /* val builder = AlertDialog.Builder(this)
+        builder.setMessage("구매 진행하시겠습니까?")
+            .setPositiveButton("예") { dialog, which ->
+                val menuIntent = Intent(this, MenuActivity::class.java)
+                startActivity(menuIntent)
+            }
+            .setNegativeButton("뒤로가기") { dialog, which ->
+
+            }
+            .show()
+    }*/
+        val inflater = LayoutInflater.from(this)
+        val customView = inflater.inflate(R.layout.custom_popup_searchingstore, null)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(customView)
+
+        val alertDialog = builder.create()
+
+        val btnYes = customView.findViewById<Button>(R.id.btnYes)
+        btnYes.setOnClickListener {
+            val menuIntent = Intent(this, MenuActivity::class.java)
+            startActivity(menuIntent)
+
+            alertDialog.dismiss()
+        }
+
+        val btnNo = customView.findViewById<Button>(R.id.btnNo)
+        btnNo.setOnClickListener {
+            // Handle 'No' button click
+            // 여기에 아무 동작 없이 팝업을 닫는 로직을 추가하세요
+
+            alertDialog.dismiss()
+        }
+        alertDialog.show()
     }
 
     override fun onPause() {
